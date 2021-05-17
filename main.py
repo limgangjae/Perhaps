b44b3fbc020959e674bafbff9d4cb3909a5531e6
hmm <84218316+limgangjae@users.noreply.github.com>
2021-05-17 20:49:31 +0900

    Initialization and running of the bot

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from keep_alive import keep_alive
 bot = commands.Bot(command_prefix='g!')
 bot.help_command = None
 
-#bot.load_extension("cogs.memory_game")
 bot.load_extension("cogs.utility")
 
 keep_alive()
