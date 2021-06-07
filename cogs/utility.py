f321ee94c92c3aea826ffd11e3dc8e80d3c02d98
hmm <84218316+limgangjae@users.noreply.github.com>
2021-06-07 14:46:06 +0900

    Changed help command to utilize subclassing

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,42 +1,60 @@
 import discord
+import random
 from discord.ext import commands
 from main import bot
+from cogs.errorhandler import rfooter
 
-def xstr(string):
-  if string is None: return ""
-  else: return string
+class myhelp(commands.HelpCommand):
 
-class Utility(commands.Cog):
+  def get_command_signature(self, command):
+    return f"{self.clean_prefix}{command.qualified_name} {command.signature}"
 
+  #the help command
+  async def send_bot_help(self, mapping: dict=None):
+    embed = discord.Embed(title="Help")
+    for cog, command in mapping.items():
+      s = [f"**{c.qualified_name}** {c.help}" for c in command]
+      #if there are commands in this cog
+      if s:
+        #using getattr() here to avoid raising an error
+        cog_name = getattr(cog, "qualified_name", "No Category")
+        embed.add_field(name=cog_name, value="\n".join(s), inline=False)
+    embed.set_footer(text=f"Requested by {str(self.context.message.author)}")
+    await self.context.reply(embed=embed)
+
+  #the command specific help command
+  async def send_command_help(self, command: str=None):
+    embed = discord.Embed(title=self.get_command_signature(command))
+    embed.add_field(name="Help", value=command.help, inline=False)
+    a = command.aliases
+    #if the command has aliases
+    if a:
+      embed.add_field(name="Aliases", value=", ".join(a), inline=False)
+    embed.set_footer(text=f"Requested by {str(self.context.message.author)}")
+    await self.context.reply(embed=embed)
+
+  #help command specific error handler
+  async def send_error_message(self, e):
+    embed = discord.Embed(title="An error occured!", description=f"```{e}```")
+    embed.set_footer(text=rfooter[random.randint(0, len(rfooter)-1)])
+    await self.context.reply(embed=embed)
+
+class utility(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.help_command = bot.help_command
+    bot.help_command = myhelp(command_attrs={"aliases": ["h"]})
+    bot.help_command.cog = self
 
-  @commands.command(aliases = ["ping"])
-  async def Ping(self, ctx):
+  @commands.command()
+  async def ping(self, ctx):
     """
-    `g!ping` gets the latency of the bot in milliseconds
+    Gets the bot's latency to Discord
     """
     await ctx.message.add_reaction("🏓")
-    await ctx.send(f"{round(1000*bot.latency)} ms")
-
-  @commands.command(aliases = ["help"])
-  async def Help(self, ctx):
-    """
-    `g!Help` shows this message
-    """
-    formatted = {}
-    for command in bot.commands:
-      original = xstr(formatted.get(command.cog_name))
-      if len(command.aliases) == 0:
-        formatted[command.cog_name] = f"{original}\n**{command}** {command.help}".strip()
-      else:
-        aliases = ", ".join(command.aliases)
-        formatted[command.cog_name] = f"{original}\n**{command}, {aliases}** {command.help}".strip()
-    embed = discord.Embed(title="Help", color=0x3498db)
-    for cog in formatted:
-      embed.add_field(name = cog, value = formatted[cog], inline = False)
-    embed.set_footer(text=f"requested by {str(ctx.message.author)}")
-    await ctx.send(embed=embed)
+    embed = discord.Embed(title="Pong!", description=f"{1000*round(bot.latency, 3)} ms")
+    embed.set_footer(text=f"Requested by {str(ctx.message.author)}")
+    await ctx.reply(embed=embed)
 
 def setup(bot):
-  bot.add_cog(Utility(bot))
+  bot.add_cog(utility(bot))
