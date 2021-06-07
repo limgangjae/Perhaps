461c5106acc2f30076c65b0d2b569025e8dc8bc2
hmm <84218316+limgangjae@users.noreply.github.com>
2021-06-07 14:44:22 +0900

    Added more cogs

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 from discord.ext import commands
 from keep_alive import keep_alive
 
-bot = commands.Bot(command_prefix='g!')
-bot.help_command = None
+bot = commands.Bot(command_prefix="p!")
 
 bot.load_extension("cogs.utility")
+bot.load_extension("cogs.admin")
+bot.load_extension("cogs.errorhandler")
 
 keep_alive()
 bot.run(os.getenv("TOKEN"))
