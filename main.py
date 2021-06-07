ba87ff91a481b6b203d18dd6354c286e2997caeb
hmm <84218316+limgangjae@users.noreply.github.com>
2021-06-07 14:58:15 +0900

    Added `Admin`, `ErrorHandler` cogs

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from keep_alive import keep_alive
 bot = commands.Bot(command_prefix="p!")
 
 bot.load_extension("cogs.utility")
+bot.load_extension("cogs.admin")
+bot.load_extension("cogs.errorhandler")
 
 keep_alive()
 bot.run(os.getenv("TOKEN"))
