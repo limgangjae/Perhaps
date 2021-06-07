56ad1c73a5dbcdb2f49abf4411bdde16aa27dfe6
hmm <84218316+limgangjae@users.noreply.github.com>
2021-06-07 14:57:47 +0900

    Added more cogs

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from keep_alive import keep_alive
 bot = commands.Bot(command_prefix="p!")
 
 bot.load_extension("cogs.utility")
-bot.load_extension("cogs.admin")
-bot.load_extension("cogs.errorhandler")
 
 keep_alive()
 bot.run(os.getenv("TOKEN"))
