27ae094cf146423acd611f3100b32df1c980a17c
Fripe <72686066+Fripe070@users.noreply.github.com>
2021-06-09 15:24:30 +0200

    Lists are a thing

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from keep_alive import keep_alive
 
 bot = commands.Bot(command_prefix="p!")
 
-bot.load_extension("cogs.utility")
-bot.load_extension("cogs.admin")
-bot.load_extension("cogs.fun")
-bot.load_extension("cogs.errorhandler")
+COGS = ["utility", "admin", "fun", "errorhandler"]
+
+for cog in COGS:
+  bot.load_extension(f"cogs.{cog}")
 
 keep_alive()
 bot.run(os.getenv("TOKEN"))
